10d417b65ece32d930b26afff1bb7f525746dbcc
zyan wu <zyan@ni.com>
2018-07-03 09:21:32 +0800

    go through get started

--- mble/app/src/main/java/com/ni/mble/MainActivity.kt
+++ mble/app/src/main/java/com/ni/mble/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.ni.mble
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val EXTRA_MESSAGE = "com.ni.mble.MESSAGE"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun sendMessage(view: View) {
+        val editText = findViewById<EditText>(R.id.editText)
+        val message = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
     }
 }
