f543d12e98033a580d6eae20c6e6098c5ffe3b46
zyan wu <zyan@ni.com>
2018-07-05 17:42:45 +0800

    location list shown correctly

--- mble/app/src/main/java/com/ni/mble/LocationActivity.java
+++ mble/app/src/main/java/com/ni/mble/LocationActivity.java
@@ -16,28 +16,39 @@ import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class LocationActivity extends AppCompatActivity {
     private ListView locationView;
 
+    class LocationInfo {
+        String name;
+        String averageRssi;
+        String scannedNum;
+        String greenNum;
+        String yellowNum;
+        String redNum;
+    }
+
     class LocationAdapter extends BaseAdapter {
         private LayoutInflater inflater;
+        private final LocationInfo locationInfos[];
 
-        public LocationAdapter() {
+        public LocationAdapter(LocationInfo locationInfos[]) {
             super();
             inflater = LocationActivity.this.getLayoutInflater();
+            this.locationInfos = locationInfos;
         }
 
         @Override
         public int getCount() {
-            return 0;
+            if (locationInfos == null) {
+                return 0;
+            }
+            return locationInfos.length;
         }
 
         @Override
         public Object getItem(int i) {
-            return null;
+            return locationInfos[i];
         }
 
         @Override
@@ -66,17 +77,25 @@ public class LocationActivity extends AppCompatActivity {
             } else {
                 viewHolder = (ViewHolder) view.getTag();
             }
+            LocationInfo info = locationInfos[i];
+            viewHolder.locationName.setText(info.name);
 
+            String eol = System.getProperty("line.separator");
+            String summary = getString(R.string.location_summary) + eol;
+            String indent = new String();
+            for (int s = 0; s < summary.length(); ++s) {
+                indent += " ";
+            }
+
+            summary += indent + info.scannedNum + " " + getString(R.string.location_scanned) + " " + eol;
+            summary += indent + info.greenNum + " " + getString(R.string.location_green) + " " + eol;
+            summary += indent + info.yellowNum + " " + getString(R.string.location_yellow) + " " + eol;
+            summary += indent + info.redNum + " " + getString(R.string.location_red);
+            viewHolder.summary.setText(summary);
+            viewHolder.averageRssi.setText(getString(R.string.location_rssi) + " " + info.averageRssi);
             return view;
-        }
-    }
 
-    class locationInfo {
-        String averageRssi;
-        String scannedNum;
-        String greenNum;
-        String yellowNum;
-        String redNum;
+        }
     }
 
     @Override
@@ -86,12 +105,13 @@ public class LocationActivity extends AppCompatActivity {
         setTitle(R.string.title_locations);
 
         String[] locationInfoNames = getIntent().getStringArrayExtra("locationInfoNames");
-        locationInfo[] locationList = new locationInfo[locationInfoNames.length];
+        LocationInfo[] locationList = new LocationInfo[locationInfoNames.length];
         for (int i = 0; i < locationInfoNames.length; ++i)
         {
             String fileName = locationInfoNames[i];
             SharedPreferences locationData = getSharedPreferences(fileName, 0);
-            locationInfo myLocation = new locationInfo();
+            LocationInfo myLocation = new LocationInfo();
+            myLocation.name = fileName;
             myLocation.averageRssi = locationData.getString("average_rssi", "Null");
             myLocation.scannedNum = locationData.getString("scanned_num", "Null");
             myLocation.greenNum = locationData.getString("green_num", "Null");
@@ -100,9 +120,8 @@ public class LocationActivity extends AppCompatActivity {
             locationList[i] = myLocation;
         }
 
-
         locationView = findViewById(R.id.locations);
-        locationView.setAdapter(new LocationAdapter());
+        locationView.setAdapter(new LocationAdapter(locationList));
     }
 
     @Override
