f52cdcd4f19f8aab787717d291196fdf1d3ab3ab
zyan wu <zyan@ni.com>
2018-07-05 16:35:22 +0800

    make location activty returnable

--- mble/app/src/main/java/com/ni/mble/LocationActivity.java
+++ mble/app/src/main/java/com/ni/mble/LocationActivity.java
@@ -1,7 +1,9 @@
 package com.ni.mble;
 
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class LocationActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class LocationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
