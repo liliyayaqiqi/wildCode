3c0c2bba296c85b4cb1ab37065f2a36a6d062cc1
zyan wu <zyan@ni.com>
2018-07-05 15:31:18 +0800

    waveform activity can return to home

--- mble/app/src/main/java/com/ni/mble/WaveformActivity.java
+++ mble/app/src/main/java/com/ni/mble/WaveformActivity.java
@@ -4,10 +4,12 @@ import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.graphics.drawable.GradientDrawable;
 import android.media.tv.TvContract;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -141,5 +143,15 @@ public class WaveformActivity extends AppCompatActivity {
 
         progressBar = findViewById(R.id.acquire_progress);
         progressBar.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            NavUtils.navigateUpFromSameTask(this);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
