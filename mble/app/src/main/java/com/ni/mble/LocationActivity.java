6e2aeb31f19b7962d16d594a829b1ceb4d0978b1
zyan wu <zyan@ni.com>
2018-07-05 16:59:16 +0800

    create list view and its adapter for locations

--- mble/app/src/main/java/com/ni/mble/LocationActivity.java
+++ mble/app/src/main/java/com/ni/mble/LocationActivity.java
@@ -3,14 +3,73 @@ package com.ni.mble;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class LocationActivity extends AppCompatActivity {
+    private ListView locationView;
+
+    class LocationAdapter extends BaseAdapter {
+        private LayoutInflater inflater;
+
+        public LocationAdapter() {
+            super();
+            inflater = LocationActivity.this.getLayoutInflater();
+        }
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+
+            class ViewHolder {
+                TextView locationName;
+                TextView averageRssi;
+                TextView summary;
+            }
+
+            ViewHolder viewHolder;
+            // General ListView optimization code.
+            if (view == null) {
+                view = inflater.inflate(R.layout.location, null);
+                viewHolder = new ViewHolder();
+                viewHolder.locationName = view.findViewById(R.id.location_name);
+                viewHolder.summary = view.findViewById(R.id.location_summary);
+                viewHolder.averageRssi = view.findViewById(R.id.location_rssi);
+                view.setTag(viewHolder);
+            } else {
+                viewHolder = (ViewHolder) view.getTag();
+            }
+
+            return view;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+
+        locationView = findViewById(R.id.locations);
+        locationView.setAdapter(new LocationAdapter());
     }
 
     @Override
