459b7a189677e1c7f29250543b419a4064c2a180
zyan wu <zyan@ni.com>
2018-07-05 10:03:34 +0800

    add waveform activity

---
+++ mble/app/src/main/java/com/ni/mble/WaveformActivity.java
@@ -0,0 +1,13 @@
+package com.ni.mble;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WaveformActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_waveform);
+    }
+}
